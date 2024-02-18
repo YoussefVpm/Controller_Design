9bd45f5974aa5ce93dc944a845761f86af42a54a
Youssef VPM <youssefvepouyoum@gmail.com>
2024-02-18 14:05:22 +0600

    update PID design 2

--- [02] Matlab/script.m
+++ [02] Matlab/script.m
@@ -13,7 +13,7 @@
 
 %% Transfer Function
 
-close all; clc;
+close all; clc; clear;
 
 % SISO tool
 % Constants of Transfer Function
@@ -47,7 +47,46 @@ disp(sys);
 % sisotool(sys);
 % pidtool(sys)
 
-%% Export from Design to TF
+%% For iteration procedure
+% Design 1
+% Ziegler_NicholsFrequencyResponse
+% SkogestadIMC
+% chien_Hrone_Reswick
+
+close all;
+
+path = 'E:\[003] Undergrad\7TH SEMESTER\Bachelor Thesis\Controller_Design\[02] Matlab\PID design 2.mat';
+load(path);
+designs = ControlSystemDesignerSession.DesignerData;
+
+for i = 1:4
+    pid = tf(designs.Designs(i).Data.C);
+    numerator_pid = pid.Numerator;
+    denominator_pid = pid.Denominator;
+    gains = pid.Numerator{1};
+    
+    pid_tf = tf(numerator_pid, denominator_pid);
+
+    % closed & open loop TF
+    sys_OP = series(pid_tf,sys);
+    sys_CL = feedback(sys_OP, 1);
+    
+    % plot
+    figure
+    grid on
+    subplot(3,1,1);
+    step(sys_CL,'.-');
+    subplot(3,1,2);
+    nyquist(sys_CL,'r-');
+    subplot(3,1,3);
+    bodemag(1 / (1 + sys_CL),'g.-');
+    hold on
+    newPosition = [-425+(i/0.99)*450, 50, 450, 900]; % [left, bottom, width, height]
+    set(gcf, 'Position', newPosition,'Name','Design');
+end
+
+
+%% Manual Procedure
 % load designs
 path = 'E:\[003] Undergrad\7TH SEMESTER\Bachelor Thesis\Controller_Design\[02] Matlab\PID design.mat';
 load(path);
@@ -88,6 +127,12 @@ pid_tf = tf(numerator_pid, denominator_pid);
 sys_OP = series(pid_tf,sys);
 sys_CL = feedback(sys_OP, 1);
 
+% Extract PID gains for simulink
+% PID gains
+new_kp = gains(2);
+new_ki = gains(3);
+new_kd = gains(1);
+
 %% Responds Analysis
 close all;
 
@@ -105,20 +150,6 @@ title('Nyquist Plot');
 figure;
 step(sys_CL);
 title('Step response');
-
-
-%% Extract PID gains for simulink
-% PID gains
-new_kp = gains(2);
-new_ki = gains(3);
-new_kd = gains(1);
-
-% Alternative
-% a = 0.014898;
-% b = 2.073;
-% new_kp = a*b;
-% new_ki = a*b^2;
-% new_kd = a;
 
 %% Data Plotting
 
@@ -145,3 +176,4 @@ title('PID Reference');
 subplot(2, 2, 4);
 plot(out.PID.time, out.PID.signals(4).values);
 title('PID Ouput');
+
