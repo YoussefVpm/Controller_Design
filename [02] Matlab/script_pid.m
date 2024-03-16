6de3ba4b6d9b9a1da154c785e628ae79b2cb3aaf
Youssef VPM <youssefvepouyoum@gmail.com>
2024-03-16 12:13:36 +0600

    cascade  update

--- [02] Matlab/script_pid.m
+++ [02] Matlab/script_pid.m
@@ -57,23 +57,18 @@ disp(sys);
 % pidtool(sys)
 
 %% For iteration procedure
-% Design 1
-% Design 2 
-% Ziegler_NicholsFrequencyResponse
-% Design 4
-
+% Design 1  % Design 2  % Ziegler_NicholsFrequencyResponse  % Design 4
 close all;
 
 path = 'E:\[003] Undergrad\7TH SEMESTER\Bachelor Thesis\Controller_Design\[02] Matlab\PID design.mat';
 load(path);
 designs = ControlSystemDesignerSession.DesignerData;
 
-for i = 1:4
+for i = 1:3
     pid = tf(designs.Designs(i).Data.C);
     numerator_pid = pid.Numerator;
     denominator_pid = pid.Denominator;
     gains = pid.Numerator{1};
-    
     pid_tf = tf(numerator_pid, denominator_pid);
 
     % closed & open loop TF
@@ -89,12 +84,11 @@ for i = 1:4
     nyquist(sys_CL,'r-');
     subplot(3,1,3);
     margin(sys_OP)
-%     bodemag(1 / (1 + sys_CL),'g.-');
+    bodemag(1 / (1 + sys_CL),'g.-');
     hold on
     newPosition = [-425+(i/0.99)*450, 50, 450, 900]; % [left, bottom, width, height]
     set(gcf, 'Position', newPosition,'Name','Design');
-end
-
+end 
 
 %% Manual Procedure
 % load designs
@@ -105,7 +99,6 @@ designs = ControlSystemDesignerSession.DesignerData;
 % select design data
 designChoice = input('Enter a Controller choice (1-4): ');
 
-% PID TF
 switch designChoice
     case 1
         pid_1 = tf(designs.Designs(1).Data.C);
@@ -178,70 +171,155 @@ step(sys_CL,'.-');
 title('Step response');
 
 %% plots for thesis book
-close all
 
-hfig = figure;
-margin(sys_OP);
+close all;
 
-h = findall(gcf,'Type','line');
+path = 'E:\[003] Undergrad\7TH SEMESTER\Bachelor Thesis\Controller_Design\[02] Matlab\PID design.mat';
+load(path);
+designs = ControlSystemDesignerSession.DesignerData;
 
-% Modify the linewidth of the lines
-newLinewidth = 2;  % Adjust this value as needed
-for i = 1:length(h)
-    set(h(i), 'LineWidth', newLinewidth);
+for i = 1:3
+    pid = tf(designs.Designs(i).Data.C);
+    numerator_pid = pid.Numerator;
+    denominator_pid = pid.Denominator;
+    gains = pid.Numerator{1};
+    
+    pid_tf = tf(numerator_pid, denominator_pid);
+
+    % closed & open loop TF
+    sys_OP = series(pid_tf,sys);
+    sys_CL = feedback(sys_OP, 1);
+
+    hfig = figure;
+    step(sys_CL);
+    axis([0 0.12 0 1.7]);
+    grid on
+    xlabel('Time');
+    ylabel('Amplitude, Y(t) (mm)');
+    title(['Unit Step Response of Design ' num2str(i)]);
+    legend(['Design ' num2str(i)]);
+    
+    step_info = stepinfo(sys_CL);
+
+    % Extract required metrics
+    percentage_overshoot = step_info.Overshoot;
+    rise_time = step_info.RiseTime;
+    settling_time = step_info.SettlingTime;
+    [response, time] = step(sys_CL);
+    steady_state_error = 1 - response(end);
+
+    text(0.04, 0.6, [' Steady state error = ' num2str(steady_state_error) ' mm'], 'Color', '[0 0.4470 0.7410]');
+    text(0.04, 0.5, [' Rise time = ' num2str(rise_time) ' s'], 'Color', '[0 0.4470 0.7410]');
+    text(0.04, 0.4, [' Settling time = ' num2str(settling_time) ' s'], 'Color', '[0 0.4470 0.7410]');
+    text(0.04, 0.3, [' \% OS = ' num2str(percentage_overshoot) ' \%'], 'Color', '[0 0.4470 0.7410]');
+    
+    h = findall(gcf,'Type','line');
+
+    newLinewidth = 2;  % Adjust this value as needed
+    for j = 1:length(h)
+        set(h(j), 'LineWidth', newLinewidth);
+    end
+
+    pictureWidth = 15;
+    hw_ratio = 0.65;
+    set(findall(hfig, '-property', 'Fontsize'), 'Fontsize', 12)
+    set(findall(hfig, '-property', 'Box'), 'Box', 'on')
+    set(findall(hfig, '-property', 'Interpreter'), 'Interpreter', 'latex')
+    set(findall(hfig, '-property', 'TickLabelInterpreter'), 'TickLabelInterpreter', 'latex')
+    set(hfig, 'Units', 'Centimeters', 'Position', [3 3 pictureWidth hw_ratio*pictureWidth])
+    pos = get(hfig, 'Position');
+    set (hfig, 'PaperPositionMode', 'Auto', 'PaperUnits', 'centimeters','Papersize',[pos(3),pos(4)])
+    print(hfig,[num2str(i) ' Step response'],'-dpdf','-painters','-fillpage')
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    hfig = figure;
+    margin(sys_OP);
+
+    h = findall(gcf,'Type','line');
+
+    % Modify the linewidth of the lines
+    newLinewidth = 2;  % Adjust this value as needed
+    for j = 1:length(h)
+        set(h(j), 'LineWidth', newLinewidth);
+    end
+
+    pictureWidth = 15;
+    hw_ratio = 0.65;
+    set(findall(hfig, '-property', 'Fontsize'), 'Fontsize', 12)
+    set(findall(hfig, '-property', 'Box'), 'Box', 'on')
+    set(findall(hfig, '-property', 'Interpreter'), 'Interpreter', 'latex')
+    set(findall(hfig, '-property', 'TickLabelInterpreter'), 'TickLabelInterpreter', 'latex')
+    set(hfig, 'Units', 'Centimeters', 'Position', [3 3 pictureWidth hw_ratio*pictureWidth])
+    pos = get(hfig, 'Position');
+    set (hfig, 'PaperPositionMode', 'Auto', 'PaperUnits', 'centimeters','Papersize',[pos(3),pos(4)])
+    print(hfig,[num2str(i) ' margin'],'-dpdf','-painters','-fillpage')
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    hfig = figure;
+    nyquist(sys_OP);
+    axis([-1.5 1.5 -1 1]);
+    grid off
+
+    h = findall(gcf,'Type','line');
+
+    % Modify the linewidth of the lines
+    newLinewidth = 1.5;  % Adjust this value as needed
+    for j = 1:length(h)
+        set(h(j), 'LineWidth', newLinewidth);
+    end
+
+    % Plot a circle at (0,0) with radius 1
+    hold on;
+    rectangle('Position', [-1, -1, 2, 2], 'Curvature', [1, 1], 'EdgeColor', 'r', 'LineStyle', ':', 'LineWidth', 1);
+    hold off;
+
+    pictureWidth = 15;
+    hw_ratio = 0.65;
+    set(findall(hfig, '-property', 'Fontsize'), 'Fontsize', 12)
+    set(findall(hfig, '-property', 'Box'), 'Box', 'on')
+    set(findall(hfig, '-property', 'Interpreter'), 'Interpreter', 'latex')
+    set(findall(hfig, '-property', 'TickLabelInterpreter'), 'TickLabelInterpreter', 'latex')
+    set(hfig, 'Units', 'Centimeters', 'Position', [3 3 pictureWidth hw_ratio*pictureWidth])
+    pos = get(hfig, 'Position');
+    set (hfig, 'PaperPositionMode', 'Auto', 'PaperUnits', 'centimeters','Papersize',[pos(3),pos(4)])
+    print(hfig,[num2str(i) ' nyquist'],'-dpdf','-painters','-fillpage')
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    hfig = figure;
+    bode(sys_CL);
+
+    h = findall(gcf,'Type','line');
+
+    % Modify the linewidth of the lines
+    newLinewidth = 2;  % Adjust this value as needed
+    for j = 1:length(h)
+        set(h(j), 'LineWidth', newLinewidth);
+    end
+
+    pictureWidth = 15;
+    hw_ratio = 0.65;
+    set(findall(hfig, '-property', 'Fontsize'), 'Fontsize', 12)
+    set(findall(hfig, '-property', 'Box'), 'Box', 'on')
+    set(findall(hfig, '-property', 'Interpreter'), 'Interpreter', 'latex')
+    set(findall(hfig, '-property', 'TickLabelInterpreter'), 'TickLabelInterpreter', 'latex')
+    set(hfig, 'Units', 'Centimeters', 'Position', [3 3 pictureWidth hw_ratio*pictureWidth])
+    pos = get(hfig, 'Position');
+    set (hfig, 'PaperPositionMode', 'Auto', 'PaperUnits', 'centimeters','Papersize',[pos(3),pos(4)])
+    print(hfig,[num2str(i) ' Bode closed loop'],'-dpdf','-painters','-fillpage')
+
 end
-
-pictureWidth = 15;
-hw_ratio = 0.65;
-set(findall(hfig, '-property', 'Fontsize'), 'Fontsize', 12)
-set(findall(hfig, '-property', 'Box'), 'Box', 'on')
-set(findall(hfig, '-property', 'Interpreter'), 'Interpreter', 'latex')
-set(findall(hfig, '-property', 'TickLabelInterpreter'), 'TickLabelInterpreter', 'latex')
-set(hfig, 'Units', 'Centimeters', 'Position', [3 3 pictureWidth hw_ratio*pictureWidth])
-pos = get(hfig, 'Position');
-set (hfig, 'PaperPositionMode', 'Auto', 'PaperUnits', 'centimeters','Papersize',[pos(3),pos(4)])
-print(hfig,'margin','-dpdf','-painters','-fillpage')
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-hfig = figure;
-nyquist(sys_OP);
-axis([-1.5 1.5 -1 1]);
-grid off
-
-h = findall(gcf,'Type','line');
-
-% Modify the linewidth of the lines
-newLinewidth = 1.5;  % Adjust this value as needed
-for i = 1:length(h)
-    set(h(i), 'LineWidth', newLinewidth);
-end
-
-% Plot a circle at (0,0) with radius 1
-hold on;
-rectangle('Position', [-1, -1, 2, 2], 'Curvature', [1, 1], 'EdgeColor', 'r', 'LineStyle', ':', 'LineWidth', 1);
-hold off;
-
-pictureWidth = 15;
-hw_ratio = 0.65;
-set(findall(hfig, '-property', 'Fontsize'), 'Fontsize', 12)
-set(findall(hfig, '-property', 'Box'), 'Box', 'on')
-set(findall(hfig, '-property', 'Interpreter'), 'Interpreter', 'latex')
-set(findall(hfig, '-property', 'TickLabelInterpreter'), 'TickLabelInterpreter', 'latex')
-set(hfig, 'Units', 'Centimeters', 'Position', [3 3 pictureWidth hw_ratio*pictureWidth])
-pos = get(hfig, 'Position');
-set (hfig, 'PaperPositionMode', 'Auto', 'PaperUnits', 'centimeters','Papersize',[pos(3),pos(4)])
-print(hfig,'nyquist','-dpdf','-painters','-fillpage')
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Sensitivity 
 
 hfig = figure;
-Spid = 1 / (1 + sys_OP);
-bodemag(Spid);
-axis([1 3500 -35 10]);
-grid off
+Error = 1*5/(1+pid_tf*sys);
+bodemag(Error);
+% Spid = 1 / (1 + sys_OP);
+% bodemag(Spid);
+% axis([1 2000 -35 10]);
+grid on
 
 h = findall(gcf,'Type','line');
 
@@ -260,6 +338,7 @@ set(findall(hfig, '-property', 'TickLabelInterpreter'), 'TickLabelInterpreter', 
 set(hfig, 'Units', 'Centimeters', 'Position', [3 3 pictureWidth hw_ratio*pictureWidth])
 pos = get(hfig, 'Position');
 set (hfig, 'PaperPositionMode', 'Auto', 'PaperUnits', 'centimeters','Papersize',[pos(3),pos(4)])
+% print(hfig,'sensitivity','-dpdf','-painters','-fillpage')
 
 %% Data Plotting
 
@@ -276,9 +355,11 @@ load('E:\[003] Undergrad\7TH SEMESTER\Bachelor Thesis\Controller_Design\[02] Mat
 
 % Disturbance
 hfig = figure;
-plot(out_with.PID.time, out_with.PID.signals(1).values);
+plot(out_with.PID.time, out_with.PID.signals(1).values, 'LineWidth', 1);
 grid on
-title('PID Disturbance');
+xlabel('Time (s)')
+ylabel('Input disturbance (V)');
+title('Cutting Force Input Disturbance at 1500 RPM');
 
 pictureWidth = 15;
 hw_ratio = 0.65;
@@ -289,12 +370,16 @@ set(findall(hfig, '-property', 'TickLabelInterpreter'), 'TickLabelInterpreter', 
 set(hfig, 'Units', 'Centimeters', 'Position', [3 3 pictureWidth hw_ratio*pictureWidth])
 pos = get(hfig, 'Position');
 set (hfig, 'PaperPositionMode', 'Auto', 'PaperUnits', 'centimeters','Papersize',[pos(3),pos(4)])
+print(hfig,'input disturbance','-dpdf','-painters','-fillpage')
+
 
 % Reference
 hfig = figure;
-plot(out_with.PID.time, out_with.PID.signals(3).values);
+plot(out_with.PID.time, out_with.PID.signals(3).values, 'LineWidth', 2);
 grid on
-title('PID Reference');
+xlabel('Time (s)');
+ylabel('Input Reference (mm)');
+title('Input Reference');
 
 pictureWidth = 15;
 hw_ratio = 0.65;
@@ -305,16 +390,20 @@ set(findall(hfig, '-property', 'TickLabelInterpreter'), 'TickLabelInterpreter', 
 set(hfig, 'Units', 'Centimeters', 'Position', [3 3 pictureWidth hw_ratio*pictureWidth])
 pos = get(hfig, 'Position');
 set (hfig, 'PaperPositionMode', 'Auto', 'PaperUnits', 'centimeters','Papersize',[pos(3),pos(4)])
+print(hfig,'reference','-dpdf','-painters','-fillpage')
+
 
 %__________________________________ERROR_________________________________%
 
-% Error
+%________Maximum Tracking Error_________%
+
 hfig = figure;
 plot(out_with.PID.time, out_with.PID.signals(2).values, 'LineWidth', 1);
 hold on
 grid on
-title('PID Error');
-
+xlabel('Time (s)')
+ylabel('position Error With Disturbance (mm)')
+title('PID Tracking Error');
 
 xSubset = out_with.PID.time(out_with.PID.time > 0.5);
 ySubset = out_with.PID.signals(2).values(out_with.PID.time > 0.5);
@@ -341,14 +430,18 @@ set(findall(hfig, '-property', 'TickLabelInterpreter'), 'TickLabelInterpreter', 
 set(hfig, 'Units', 'Centimeters', 'Position', [3 3 pictureWidth hw_ratio*pictureWidth])
 pos = get(hfig, 'Position');
 set (hfig, 'PaperPositionMode', 'Auto', 'PaperUnits', 'centimeters','Papersize',[pos(3),pos(4)])
+print(hfig,'error with','-dpdf','-painters','-fillpage')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 hfig = figure;
 plot(out_without.PID.time, out_without.PID.signals(2).values, 'LineWidth', 2);
+axis([0 15 -0.005 0.005]);
 hold on
 grid on
-title('PID Error');
+xlabel('Time (s)')
+ylabel('position Error Without Disturbance (mm)')
+title('PID Tracking Error');
 
 
 xSubset = out_without.PID.time(out_without.PID.time > 0.5);
@@ -376,16 +469,22 @@ set(findall(hfig, '-property', 'TickLabelInterpreter'), 'TickLabelInterpreter', 
 set(hfig, 'Units', 'Centimeters', 'Position', [3 3 pictureWidth hw_ratio*pictureWidth])
 pos = get(hfig, 'Position');
 set (hfig, 'PaperPositionMode', 'Auto', 'PaperUnits', 'centimeters','Papersize',[pos(3),pos(4)])
+print(hfig,'error without','-dpdf','-painters','-fillpage')
 
-%__________________ROOT MEAN SQAURE ERROR________________________________%
+%% __________________ROOT MEAN SQAURE ERROR_______________________________%
+load('E:\[003] Undergrad\7TH SEMESTER\Bachelor Thesis\Controller_Design\[02] Matlab\PID out with.mat')
+load('E:\[003] Undergrad\7TH SEMESTER\Bachelor Thesis\Controller_Design\[02] Matlab\PID out without.mat')
 
-RMSE = sqrt(mean(out_with.PID.signals(2).values.^2));
+RMSE_with = sqrt(mean(out_with.PID.signals(2).values.^2));
+RMSE_without = sqrt(mean(out_without.PID.signals(2).values.^2));
 
-%__________________Fast Fourier Transform________________________________%
+percentage_variation = ((RMSE_with - RMSE_without)/RMSE_with)*100;
 
+
+%% _________________Fast Fourier Transform________________________________%
 
 % Example time-domain data
-Fs = 500; % Sampling frequency (Hz)
+Fs = 2000; % Sampling frequency (Hz)
 signal = out_with.PID.signals(2).values;
 
 % Perform FFT
@@ -397,17 +496,70 @@ amplitude_spectrum = 2/N * abs(fft_result(1:N/2+1));
 fft_result_2 = fft(out_without.PID.signals(2).values);
 amplitude_spectrum_2 = 2/N * abs(fft_result_2(1:N/2+1));
 
-% Plot the time-domain signal and its frequency spectrum
-figure;
-plot(frequencies, amplitude_spectrum);
-hold on 
-plot(frequencies, amplitude_spectrum_2);
-title('Frequency Spectrum');
+% Plot the frequency spectrum
+hfig = figure;
+plot(frequencies, amplitude_spectrum, 'LineWidth', 2);
+axis([0 100 0 0.004]); 
+title('FFT Tracking Error PID Without Error');
 xlabel('Frequency (Hz)');
-ylabel('Amplitude');
+ylabel('Position Error Without Disturbance (mm)');
+hold on
+
+[maxY, maxIndex] = max(amplitude_spectrum);
+maxX = frequencies(maxIndex);
+
+% Plot a red dot at the maximum point
+plot(maxX, maxY, 'ro', 'MarkerSize', 7);
+
+% Display the maximum point
+text(maxX+2, maxY, [' Amplitude = ' num2str(maxY) ' mm'], 'Color', 'red');
 
 % Show the plot
 grid on;
 
+pictureWidth = 15;
+hw_ratio = 0.65;
+set(findall(hfig, '-property', 'Fontsize'), 'Fontsize', 12)
+set(findall(hfig, '-property', 'Box'), 'Box', 'on')
+set(findall(hfig, '-property', 'Interpreter'), 'Interpreter', 'latex')
+set(findall(hfig, '-property', 'TickLabelInterpreter'), 'TickLabelInterpreter', 'latex')
+set(hfig, 'Units', 'Centimeters', 'Position', [3 3 pictureWidth hw_ratio*pictureWidth])
+pos = get(hfig, 'Position');
+set (hfig, 'PaperPositionMode', 'Auto', 'PaperUnits', 'centimeters','Papersize',[pos(3),pos(4)])
+print(hfig,'FFT without','-dpdf','-painters','-fillpage')
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+hfig = figure;
+plot(frequencies, amplitude_spectrum_2, 'LineWidth', 2);
+axis([0 100 0 0.004]);
+title('FFT Tracking Error PID With Error');
+xlabel('Frequency (Hz)');
+ylabel('Position Error With Disturbance (mm)');
+hold on
+
+[maxY, maxIndex] = max(amplitude_spectrum_2);
+maxX = frequencies(maxIndex);
+
+% Plot a red dot at the maximum point
+plot(maxX, maxY, 'ro', 'MarkerSize', 7);
+
+% Display the maximum point
+text(maxX+2, maxY, [' Amplitude = ' num2str(maxY) ' mm'], 'Color', 'red');
+
+% Show the plot
+grid on;
+% Show the plot
+grid on;
+
+pictureWidth = 15;
+hw_ratio = 0.65;
+set(findall(hfig, '-property', 'Fontsize'), 'Fontsize', 12)
+set(findall(hfig, '-property', 'Box'), 'Box', 'on')
+set(findall(hfig, '-property', 'Interpreter'), 'Interpreter', 'latex')
+set(findall(hfig, '-property', 'TickLabelInterpreter'), 'TickLabelInterpreter', 'latex')
+set(hfig, 'Units', 'Centimeters', 'Position', [3 3 pictureWidth hw_ratio*pictureWidth])
+pos = get(hfig, 'Position');
+set (hfig, 'PaperPositionMode', 'Auto', 'PaperUnits', 'centimeters','Papersize',[pos(3),pos(4)])
+print(hfig,'FFT with','-dpdf','-painters','-fillpage')
 
